bec3413c09060926318bebe4ffb82f9d78256726
Jamie Everett <jreverett2442@gmail.com>
2019-12-09 14:13:00 +0000

    Add test.js with placeholder, change test script name

---
+++ test/test.js
@@ -0,0 +1,7 @@
+var assert = require("assert");
+
+describe("Placeholder tests", function() {
+  it("Should return true", function() {
+    assert.equal(1, 1, "this is a placeholder test");
+  });
+});
